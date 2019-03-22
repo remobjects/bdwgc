176eab2152c2e7dbf43268eb1a949a18efd2691d
Carlo Kok <ck@remobjects.com>
2019-03-22 10:02:27 +0100

    0: Import fixes.

--- compileandroid.bat
+++ compileandroid.bat
@@ -3,16 +3,11 @@ set CLANG=c:\p\llvm\llvm-bin32\RelWithDebInfo\bin\clang.exe
 set LLVMAR=c:\p\llvm\llvm-bin32\RelWithDebInfo\bin\llvm-ar.exe
 
 set TRIPLE=x86_64-linux-androideabi
-set COMMONFLAGS=-O0 -c -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC -DGC_DONT_DEFINE_LINK_MAP -DPLATFORM_ANDROID  -D GC_THREADS -I libatomic_ops\src -D_REENTRANT
-set COMPILEFLAGS=%COMMONFLAGS% -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC -DGC_DONT_DEFINE_LINK_MAP -DPLATFORM_ANDROID -D GC_THREADS -I libatomic_ops\src -D_REENTRANT -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include\x86_64-linux-android 
-del *.o /s /q /f
-echo "Triple: %TRIPLE% RelWithDebInfo"
-%CLANG% %COMPILEFLAGS% -target %TRIPLE% alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c extra\msvc_dbg.c pthread_support.c pthread_stop_world.c pthread_start.c
-%LLVMAR% -format=gnu rcs libgc-x86_64.a alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o msvc_dbg.o pthread_support.o pthread_stop_world.o pthread_start.o
+set COMMONFLAGS=-O3 -c -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC -DNO_EXECUTE_PERMISSION -DGC_DONT_DEFINE_LINK_MAP -DPLATFORM_ANDROID  -D GC_THREADS -I libatomic_ops\src -D_REENTRANT
 
 
 
-set TRIPLE=i686-linux-android -mcpu=generic
+set TRIPLE=i686-linux-android -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32 -mstackrealign
 set COMPILEFLAGS=%COMMONFLAGS% -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC -DGC_DONT_DEFINE_LINK_MAP -DPLATFORM_ANDROID -D GC_THREADS -I libatomic_ops\src -D_REENTRANT -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include\x86_64-linux-android 
 del *.o /s /q /f
 echo "Triple: %TRIPLE% RelWithDebInfo"
@@ -23,7 +18,7 @@ echo "Triple: %TRIPLE% RelWithDebInfo"
 
 
 
-set TRIPLE=x86_64-linux-android -mcpu=generic
+set TRIPLE=x86_64-linux-android -march=x86-64 -msse4.2 -mpopcnt -m64 -mtune=intel
 set COMPILEFLAGS=%COMMONFLAGS% -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC -DGC_DONT_DEFINE_LINK_MAP -DPLATFORM_ANDROID -D GC_THREADS -I libatomic_ops\src -D_REENTRANT -I C:\Users\carlokok\AppData\Local\Android\sdk\ndk-bundle\sysroot\usr\include\x86_64-linux-android 
 del *.o /s /q /f
 echo "Triple: %TRIPLE% RelWithDebInfo"
