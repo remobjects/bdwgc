3b2eaa788bc9250725b3f4dbcc0803611b5f1436
Carlo Kok <ck@remobjects.com>
2020-06-24 11:11:42 +0200

    0: Also compile ARM64 osx.

--- compile.bat
+++ compile.bat
@@ -11,6 +11,35 @@ del *.lib /s /q /f
 set COMMONFLAGS= -Wno-deprecated-declarations -Wno-nullability-completeness -Wno-unknown-attributes -Wno-builtin-requires-header -D HAVE_DL_ITERATE_PHDR -D GC_THREADS -I libatomic_ops\src -Wno-expansion-to-defined -Wno-ignored-attributes
 
 
+
+
+
+set TRIPLE=arm64-apple-macosx
+set COMPILEFLAGS=-O0 -g -c %OSXINC% -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC %COMMONFLAGS%  -D_REENTRANT
+del *.o /s /q /f
+echo "Triple: %TRIPLE% Debug"
+%CLANG% %COMPILEFLAGS% -target %TRIPLE% alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c extra\msvc_dbg.c pthread_support.c darwin_stop_world.c pthread_start.c
+%LLVMAR% -format=gnu rcs OSX_aarch64Debug.lib alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o msvc_dbg.o pthread_support.o darwin_stop_world.o pthread_start.o
+
+set TRIPLE=arm64-apple-macosx
+set COMPILEFLAGS=-O3 -c %OSXINC% -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC %COMMONFLAGS%  -D_REENTRANT
+del *.o /s /q /f
+echo "Triple: %TRIPLE% Release"
+%CLANG% %COMPILEFLAGS% -target %TRIPLE% alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c extra\msvc_dbg.c pthread_support.c darwin_stop_world.c pthread_start.c
+%LLVMAR% -format=gnu rcs OSX_gcaarch64Release.lib alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o msvc_dbg.o pthread_support.o darwin_stop_world.o pthread_start.o
+
+set TRIPLE=arm64-apple-macosx
+set COMPILEFLAGS=-O3 -g -c %OSXINC% -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC %COMMONFLAGS%  -D_REENTRANT
+del *.o /s /q /f
+echo "Triple: %TRIPLE% RelWithDebInfo"
+%CLANG% %COMPILEFLAGS% -target %TRIPLE% alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c extra\msvc_dbg.c pthread_support.c darwin_stop_world.c pthread_start.c
+%LLVMAR% -format=gnu rcs OSX_gcaarch64RelWithDebInfo.lib alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o msvc_dbg.o pthread_support.o darwin_stop_world.o pthread_start.o
+
+
+
+
+
+
 set TRIPLE=x86_64-apple-macosx
 set COMPILEFLAGS=-O0 -g -c %OSXINC% -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC %COMMONFLAGS%  -D_REENTRANT
 del *.o /s /q /f
@@ -31,6 +60,8 @@ del *.o /s /q /f
 echo "Triple: %TRIPLE% RelWithDebInfo"
 %CLANG% %COMPILEFLAGS% -target %TRIPLE% alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c extra\msvc_dbg.c pthread_support.c darwin_stop_world.c pthread_start.c
 %LLVMAR% -format=gnu rcs OSX_gc64RelWithDebInfo.lib alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o msvc_dbg.o pthread_support.o darwin_stop_world.o pthread_start.o
+
+
 
 
 set TRIPLE=i686-pc-windows-msvc
--- outer.bat
+++ outer.bat
@@ -1,8 +1,8 @@
 @echo off
-set CLANG=c:\p\llvm\llvm-bin32\RelWithDebInfo\bin\clang.exe
-set LLVMAR=c:\p\llvm\llvm-bin32\RelWithDebInfo\bin\llvm-ar.exe
+set CLANG=C:\p\llvm-project-bin32\RelWithDebInfo\bin\clang.exe
+set LLVMAR=C:\p\llvm-project-bin32\RelWithDebInfo\bin\llvm-ar.exe
 set WINDOWSINC=
 set LINUXINC=-I C:\projects\imports\ubuntu\include\x86_64-linux-gnu -I C:\projects\imports\ubuntu\include
 set RPIINC=-I C:\projects\imports\rpi\inc\arm-linux-gnueabihf -I C:\projects\imports\rpi\inc
-set OSXINC=-I C:\projects\hi\xcode10-macos10.14\MacOSX.sdk\usr\include
+set OSXINC=-I C:\projects\hi\MacOSX.sdk\usr\include
 compile.bat
